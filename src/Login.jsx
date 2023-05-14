2ce3f948d26d48427e7b168701992f462c07cd39
AB050 <abhinayaman94@gmail.com>
2023-05-14 15:37:41 +0530

    Spotify Clone

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css'
+import {accessUrl} from "./Spotify.jsx"
+
+export default function Login() {
+  return (
+    <div className='login'>
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+    alt="Spotify" />
+     <a href={accessUrl}>LOGIN TO SPOTIFY</a>
+    </div>
+  )
+}
